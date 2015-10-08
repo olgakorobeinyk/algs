8222efef975cd3cb49bc63dcde04378d3e1fdb80
Olga Korobeinyk <okorobeinyk@cogniance.com>
2015-10-08 13:34:12 +0300

    selection, insertion, bubble sorts

--- src/main/java/ElementarySort.java
+++ src/main/java/ElementarySort.java
@@ -2,23 +2,60 @@
 public class ElementarySort {
 
     public void selection(int[] array) {
+
         int min, minIndex;
 
-        for (int i = 0; i < array.length; i ++) {
+        for (int i = 0; i < array.length - 1; i ++) {
             min = array[i];
             minIndex = i;
-            for (int j = i+1; j < array.length; j ++) {
-                if (array[j] < min) {
+            for (int j = i + 1; j <array.length; j ++) {
+                if (array[j] < min){
                     min = array[j];
                     minIndex = j;
                 }
             }
-            int temp = array[i];
-            array[i] = array[minIndex];
-            array[minIndex] = temp;
+            array[minIndex] = array[i];
+            array[i] = min;
         }
-
     }
 
+    public void insertion(int[] array) {
+
+        for (int i = 1; i < array.length; i ++){
+            int temp = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > temp) {
+                array[j+1] = array[j];
+                j --;
+            }
+            array[j+1] = temp;
+        }
+
+        /*for (int i = 1; i < array.length; i ++) {
+            for (int j = i-1; j >= 0; j --) {
+                if (array[j+1] < array[j]) {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                } else {
+                    break;
+                }
+            }
+        }*/
+    }
+
+    public void bubble(int[] array) {
+
+        for (int i = 0; i < array.length - 1; i ++) {
+            for (int j = i + 1; j < array.length; j ++) {
+                if (array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+
+                }
+            }
+        }
+    }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] a = {-4, 99, 4, 4, 1, 66, 11, 10567, 100, 48, 24, 83, 31, 11, 9, 76, 66};
+        int[] a = {101, -34, 0, 2, 99, 4, 4, 1, 66, 11, 100, 48, 24, 83, 31, 11, 9, 76, 66};
 
         ElementarySort sort = new ElementarySort();
-        sort.selection(a);
+        sort.bubble(a);
 
         for (int i = 0; i < a.length; i ++) {
             System.out.println(a[i]);
